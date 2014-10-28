76feceea9800426fa4aa6e86777ecd06e5b55f8d
Jonathan Prince <jonathan.prince@gmail.com>
2014-10-28 21:57:02 +0100

    add comments

--- resizer.php
+++ resizer.php
@@ -8,20 +8,29 @@ Class resizer {
     private $imageIn;
     private $imageOut;
 
+    /**
+     * Contruct new resizer object
+     * @param path $imgToResize location of original image
+     */
     public function __construct($imgToResize) {
 
         $this->imageInfo = getimagesize($imgToResize);
         $this->type = $this->imageInfo['mime'];
 
-        // set original dimensions
+        // set original image dimensions
         $this->inputWidth = $this->imageInfo[0];
         $this->inputHeight = $this->imageInfo[1];
 
-        // open image to be resized
+        // load image data to be resized
         $this->imageIn = $this->load($imgToResize);
 
     }
 
+    /**
+     * loads original image
+     * @param path $imgToResize location of original image
+     * @return image identifier representing the original image
+     */
     private function load($imgToResize) {
 
         switch ($this->type) {
@@ -49,12 +58,22 @@ Class resizer {
 
     }
 
+    /**
+     * Create new image with desired size
+     * @param integer $outputWidth new image width
+     * @param integer $outputHeight new image height
+     */
     public function resize($outputWidth, $outputHeight) {
 
         $this->imageOut = imagecreatetruecolor($outputWidth, $outputHeight);
         imagecopyresampled($this->imageOut, $this->imageIn, 0, 0, 0, 0, $outputWidth, $outputHeight, $this->inputWidth, $this->inputHeight);
     }
 
+    /**
+     * Save new image
+     * @param location $target path and filename for new image
+     * @param integer $quality quality level to use when saving new image
+     */
     public function save($target, $quality) {
 
         switch ($this->type) {
